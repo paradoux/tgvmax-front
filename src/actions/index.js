2cba83d3246529a28670037b3750a144fa5c92c2
Axel Martin <martin.axe@live.fr>
2018-05-02 17:01:32 +0200

    Fix sort api's results to ascendant

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API_key = "dcb084c0-061c-4a14-90bd-f63b39a115b8";
 
-const ROOT_URL = `https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&rows=30&sort=date&facet=date&facet=origine&facet=destination&refine.origine=`
+const ROOT_URL = `https://data.opendatasoft.com/api/records/1.0/search/?dataset=tgvmax%40datasncf&rows=100&sort=-date&facet=date&facet=destination&facet=origine&refine.origine=`
 
 
 export const FETCH_TRAIN = "FETCH_TRAIN"
